7f9aa128405b3dd624fd0b45bf8b718779a1148a
Akshada1901 <akshada1901@gmail.com>
2026-04-24 15:37:42 +0530

    Added UC4 - Ordered Train Consist using LinkedList

---
+++ app/src/UC4.java
@@ -0,0 +1,35 @@
+import java.util.LinkedList;
+
+public class UC4 {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        // Create LinkedList for ordered bogies
+        LinkedList<String> train = new LinkedList<>();
+
+        // Add bogies
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("AC");
+        train.add("Cargo");
+        train.add("Guard");
+
+        System.out.println("Initial Train Consist:");
+        System.out.println(train);
+
+        // Insert Pantry Car at position 2
+        train.add(2, "Pantry");
+
+        System.out.println("\nAfter adding Pantry at position 2:");
+        System.out.println(train);
+
+        // Remove first and last bogie
+        train.removeFirst();
+        train.removeLast();
+
+        System.out.println("\nAfter removing first and last bogie:");
+        System.out.println(train);
+    }
+}
