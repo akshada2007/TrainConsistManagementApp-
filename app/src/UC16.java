39937d562215da0376768a4d18a3ce7a76c857c9
Akshada1901 <akshada1901@gmail.com>
2026-04-24 16:19:20 +0530

    Added UC16 - Bubble Sort for passenger bogie capacities

---
+++ app/src/UC16.java
@@ -0,0 +1,33 @@
+public class UC16 {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        // Passenger bogie capacities
+        int[] capacities = {72, 56, 24, 70, 60};
+
+        // Bubble Sort
+        int n = capacities.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+
+                // Compare adjacent elements
+                if (capacities[j] > capacities[j + 1]) {
+
+                    // Swap
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
+            }
+        }
+
+        // Display sorted array
+        System.out.println("Sorted Capacities:");
+        for (int cap : capacities) {
+            System.out.print(cap + " ");
+        }
+    }
+}
